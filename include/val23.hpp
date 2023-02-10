1c7c3559da831d71b3399a5ac7a9c9d2f643ed57
Everett Gaius Vergara <evergara@shinra.com.ph>
2023-02-11 01:28:01 +0800

    misc edits

--- include/val23.hpp
+++ include/val23.hpp
@@ -13,25 +13,9 @@ namespace eg
     class val23 : public video 
     {
     private:
-        // const Sint32 cx = 1024 / 2;
-        // const Sint32 cy = 768 / 2;
-
         std::vector<std::unique_ptr<heart_anim>> hearts_;
-
-        // FP rot = 0.0;
-        // FP rot_n = 0.025;
-        // FP rot_min = -1.0;
-        // FP rot_max = +1.0;
-
-        // FP rad = 40.0;
-        // FP rad_n = 1.0;
-        // FP rad_min = 25.0;
-        // FP rad_max = 80.0;
-
-
         std::vector<Uint32> heart_pal_;
         std::vector<Uint8> heart_surface_;
-
 
     public:
 
@@ -50,14 +34,24 @@ namespace eg
             SDL_FillRect(surface, NULL, SDL_MapRGBA(surface->format, 0, 0, 0, 255));
             
             // Generate Palette
+            // auto heart_pal = get_palette_gradient(
+            //                     surface->format, {
+            //                         {0,     SDL_MapRGBA(surface->format, 0, 0, 0, 255)},
+            //                         {50,    SDL_MapRGBA(surface->format, 100, 0, 0, 255)},
+            //                         {100,    SDL_MapRGBA(surface->format, 255, 174, 0, 255)},
+            //                         {150,    SDL_MapRGBA(surface->format, 255, 255, 0, 255)},
+            //                         {255,    SDL_MapRGBA(surface->format, 255, 255, 255, 255)},
+            //                         });
+
             auto heart_pal = get_palette_gradient(
                                 surface->format, {
                                     {0,     SDL_MapRGBA(surface->format, 0, 0, 0, 255)},
-                                    {50,    SDL_MapRGBA(surface->format, 100, 0, 0, 255)},
-                                    {100,    SDL_MapRGBA(surface->format, 255, 174, 0, 255)},
+                                    {50,    SDL_MapRGBA(surface->format, 50, 0, 50, 255)},
+                                    {100,    SDL_MapRGBA(surface->format, 255, 0, 100, 255)},
                                     {150,    SDL_MapRGBA(surface->format, 255, 255, 0, 255)},
                                     {255,    SDL_MapRGBA(surface->format, 255, 255, 255, 255)},
                                     });
+
 
             heart_pal_ = std::move(heart_pal.value());
 
@@ -75,7 +69,6 @@ namespace eg
             hearts_.emplace_back(std::make_unique<heart_anim>(
                                     1440, 255, 3,
                                     1024 / 2 + 1024 / 4, 768 / 3,
-                                    
                                     0, 0.0625, -M_PI2, +M_PI2,
                                     40.0, -1, 10.0, 50.0));
 
@@ -85,34 +78,16 @@ namespace eg
                                     0, -0.0625, -4.0, +4.0,
                                     10.0, +5, 10.0, 50.0));
 
-
         }
 
         auto update() -> void override
         {
-
             // Get handle to surface
             auto surface = SDL_GetWindowSurface(win_);
 
             // Animate hearts
             for (auto &h : hearts_)
                 h->animate(heart_surface_, surface->w);
-
-
-            // Add Hell
-            // auto last_row = (surface->h - 2) * surface->w;
-            // for (int i = 0; i < surface->w; ++i)
-            // {
-            //     heart_surface_.at(last_row + i) = rand() % 200;
-            // }
-
-            // // Add Hell Fire
-            // int a = 12345, b = 1103515245, rnd = a;
-            // for (int i = 0; i < 1000; ++i)
-            // {
-            //     heart_surface_.at(rnd % heart_surface_.size()) = rand() % 100;
-            //     rnd = (b * rnd + 1);
-            // }
 
             // Fire effect
             auto e = (surface->h * surface->w) - surface->w - surface->w;
@@ -130,7 +105,5 @@ namespace eg
                 heart_surface_.at(i) = new_c;
             }
         }
-
     };
-
 }
